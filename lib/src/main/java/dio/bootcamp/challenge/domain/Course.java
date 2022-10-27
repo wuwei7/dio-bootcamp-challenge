97177c0fc28395b7fb0b4b57e215b2e853247472
Brandon Campos <16386564+wuwei7@users.noreply.github.com>
2022-10-27 15:42:51 -0300

    Create methods

--- lib/src/main/java/dio/bootcamp/challenge/domain/Course.java
+++ lib/src/main/java/dio/bootcamp/challenge/domain/Course.java
@@ -5,5 +5,28 @@ public class Course {
 	private String title;
 	private String description;
 	private int courseload;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getCourseload() {
+		return courseload;
+	}
+	public void setCourseload(int courseload) {
+		this.courseload = courseload;
+	}
+	@Override
+	public String toString() {
+		return "Course [title=" + title + ", description=" + description + ", courseload=" + courseload + "]";
+	}
 
 }
