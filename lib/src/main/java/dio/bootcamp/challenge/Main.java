b0dd7caf730576a1e1ed961e5b2742924b450fd1
Brandon Campos <16386564+wuwei7@users.noreply.github.com>
2022-12-01 22:23:13 -0300

    Create devs

--- lib/src/main/java/dio/bootcamp/challenge/Main.java
+++ lib/src/main/java/dio/bootcamp/challenge/Main.java
@@ -2,7 +2,9 @@ package dio.bootcamp.challenge;
 
 import java.time.LocalDate;
 
+import dio.bootcamp.challenge.domain.Bootcamp;
 import dio.bootcamp.challenge.domain.Course;
+import dio.bootcamp.challenge.domain.Dev;
 import dio.bootcamp.challenge.domain.Mentorship;
 
 public class Main {
@@ -24,9 +26,35 @@ public class Main {
 		javaMentorship.setDescription("In-depth OOP");
 		javaMentorship.setDate(LocalDate.now());
 		
-		System.out.println(javaCourse);
-		System.out.println(oopJavaCourse);
-		System.out.println(javaMentorship);
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setName("Java Bootcamp");
+		bootcamp.setDescription("The best Java bootcamp ever!");
+		bootcamp.getContents().add(javaCourse);
+		bootcamp.getContents().add(oopJavaCourse);
+		bootcamp.getContents().add(javaMentorship);
+		
+		Dev devGaladriel = new Dev();
+		devGaladriel.setName("Galadriel");
+		devGaladriel.enrollBootcamp(bootcamp);
+		System.out.println("Galadriel's enrolled contents: " + devGaladriel.getEnrolledContents());
+		devGaladriel.progress();
+		devGaladriel.progress();
+		System.out.println("Galadriel's enrolled contents: " + devGaladriel.getEnrolledContents());
+		System.out.println("Galadriel's finished contents: " + devGaladriel.getFinishedContents());
+		System.out.println("XP: " + devGaladriel.calculateTotalXp());
+
+		System.out.println("-----");
+		
+		Dev devHalbrand = new Dev();
+		devHalbrand.setName("Halbrand");
+		devHalbrand.enrollBootcamp(bootcamp);
+		System.out.println("Halbrand's enrolled contents: " + devHalbrand.getEnrolledContents());
+		devHalbrand.progress();
+		devHalbrand.progress();
+		devHalbrand.progress();
+		System.out.println("Halbrand's enrolled contents: " + devHalbrand.getEnrolledContents());
+		System.out.println("Halbrand's finished contents: " + devHalbrand.getFinishedContents());
+		System.out.println("XP: " + devHalbrand.calculateTotalXp());
 
 	}
 
